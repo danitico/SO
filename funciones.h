9b6e60a12ff0a516441c08acb9bda776f0927b9f
danitico <i62rapad@uco.es>
2017-10-30 18:23:13 +0100

    correcto

--- funciones.h
+++
@@ -1,9 +0,0 @@
-#ifndef CABECERA
-#define CABECERA
-int *btov(char *fichero, int n);
-void mostrarvector(int *v, int n);
-int compararenteros(const void *e1, const void *e2);
-void vtot(char *fichero, int *v, int n);
-int contar(char *fichero);
-void liberar(int **v);
-#endif 
