d4c2d5c75deec45b3ff9c6cf48478f1b543cee8f
Daniel Ranchal <danitico98@gmail.com>
2017-12-03 18:59:59 +0100

    memory leak

--- P4/algoritmo_RR.c
+++ P4/algoritmo_RR.c
@@ -11,115 +11,95 @@ typedef struct params{
     int t_ret;
     int t_esp;    
 }parametros;
-typedef struct nodo{
-	parametros dato;
-	struct nodo* sig;
-} node_t;
-void enqueue(node_t **head, parametros val);
-parametros dequeue(node_t **head);
-int select_i(parametros a);
+int ciclos_ejecucion_0(parametros *v);
+void procesos_listos(parametros *v, parametros *procesos, int ciclos);
+void rol_fifo(parametros *v);
+void estadistica(parametros v, parametros *p, int ciclos);
 int main(){
-	int i, x=1, j=1;
-	parametros aux;
-	node_t *head=NULL;
-	int ciclos_restantes[NPROC];
-	int ciclos;
-
+	int i;
+	int ciclos=0;
+	parametros *v=(parametros*)malloc(sizeof(parametros));
+	
 	// Inicialización de los datos de procesos
 	parametros procesos[NPROC];    
 	strcpy(procesos[0].nombre, "A");
 	procesos[0].t_ejec = 8;
 	procesos[0].t_lleg = 0;
-	enqueue(&head, procesos[0]);
-
-	
+		
 	strcpy(procesos[1].nombre, "B");
 	procesos[1].t_ejec = 4;
 	procesos[1].t_lleg = 1;
-	//enqueue(&head, procesos[1]);
-
+	
 	strcpy(procesos[2].nombre, "C");
 	procesos[2].t_ejec = 9;
 	procesos[2].t_lleg = 2;
-	//enqueue(&head, procesos[2]);
-
+	
 	strcpy(procesos[3].nombre, "D");
 	procesos[3].t_ejec = 5;
 	procesos[3].t_lleg = 3;
-	//enqueue(&head, procesos[3]);
-
+	
 	strcpy(procesos[4].nombre, "E");
 	procesos[4].t_ejec = 2;
 	procesos[4].t_lleg = 4;
-	//enqueue(&head, procesos[4]);
-
+	
 	printf("\nProceso | t_comienzo | t_final | t_retorno | t_espera\n");
 	printf("-------------------------------------------------------\n");
-	while(head!=NULL){
-		aux=dequeue(&head);
-		i=aux.t_lleg;
-		ciclos_restantes[i]=aux.t_ejec;
-		ciclos_restantes[i]-=3;
 	
-		if(ciclos_restantes[i]<0){
-			ciclos+=(ciclos_restantes[i] + 3);
-			if(i==0){
-				procesos[i].t_com = 0;
-			}
-			procesos[i].t_fin = ciclos;
-			procesos[i].t_com = ciclos - procesos[i].t_ejec;
-			procesos[i].t_ret = procesos[i].t_fin - procesos[i].t_lleg;
-			procesos[i].t_esp = procesos[i].t_ret - procesos[i].t_ejec;
-		}
-		else{
-			ciclos+=3;
-			if(j!=5){
-				x=j;
-				while(x<NPROC){
-					if(procesos[x].t_lleg <= ciclos){
-						enqueue(&head, procesos[x]);
-						j++;
-					}
-				}
-					x++;
-			}
-			else{
-			enqueue(&head, aux);
-			}
-		}
+	while(ciclos_ejecucion_0(v)==0){
+		procesos_listos(v, procesos, ciclos);
+		rol_fifo(v);
+		v[0].t_ejec-=3;
+
+		if(v[0].t_ejec < 0){estadistica(v[0], procesos, ciclos);}
+		//printf("hey-->%i\n", ciclos_ejecucion_0(v));
 	}
 	for(int i=0; i<NPROC; i++){
 			printf("   %s \t    %d \t\t %d \t   %d \t      %d\n", procesos[i].nombre,procesos[i].t_com, procesos[i].t_fin, procesos[i].t_ret, procesos[i].t_esp);
 	}
+	free(v);
 }
-void enqueue(node_t **head, parametros val){
-	node_t *new_node=malloc(sizeof(node_t));
+int ciclos_ejecucion_0(parametros *v){
 	
-	new_node->dato=val;
-	new_node->sig=*head;
-
-	*head=new_node;
+	int n=sizeof(*v)/sizeof(parametros);
+	//printf("hola->%i\n", n);
+	if(n==5){
+		for(int i=0; i<n; i++){
+			if(v[i].t_ejec!=0){return 0;}
+		}
+		return 1;
+	}
+	else{return 0;}
 }
-parametros dequeue(node_t **head){
-	node_t *current, *prev=NULL;
-	parametros retval;
-
-	if(*head==NULL){return retval;}
-
-	current=*head;
-	while(current->sig!=NULL){
-		prev=current;
-		current=current->sig;
+void procesos_listos(parametros *v, parametros *procesos, int ciclos){
+	int n=sizeof(*v)/sizeof(parametros);	
+	//printf("hola->%i\n", n);
+	for(int i=0; i<NPROC; i++){
+		if(ciclos==procesos[i].t_lleg){
+			v=(parametros*)realloc(v, (n+1)*sizeof(parametros));		
+			v[n]=procesos[i];
+			v[n].t_com=ciclos;
+		}
 	}
+}
+void rol_fifo(parametros *v){
+	int n=sizeof(*v)/sizeof(parametros);
+	//printf("hola->%lu\n", sizeof(parametros));
 
-	retval=current->dato;
-	free(current);
-
-	if(prev){
-		prev->sig=NULL;
+	if(n>1){
+		parametros aux=v[0];
+		for(int i=0; i<(n-1); i++){
+			v[i]=v[i+1];
+		}
+		v[n-1]=aux;
 	}
-	else{
-		*head=NULL;
+}
+void estadistica(parametros v, parametros *p, int ciclos){		
+	for(int i=0; i<NPROC; i++){
+		if(v.nombre[0]==p[i].nombre[0]){
+			p[i].t_com=v.t_com;
+			p[i].t_fin=ciclos;
+			p[i].t_ret=p[i].t_fin - p[i].t_lleg;
+			p[i].t_esp=p[i].t_ret - p[i].t_ejec;
+		}
 	}
-	return retval;
 }
