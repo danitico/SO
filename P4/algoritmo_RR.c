19b9fd321d828e0821876f26f5ff44ffc378f098
Daniel Ranchal <danitico98@gmail.com>
2017-12-04 11:01:58 +0100

    mismos errores

--- P4/algoritmo_RR.c
+++ P4/algoritmo_RR.c
@@ -11,15 +11,14 @@ typedef struct params{
     int t_ret;
     int t_esp;    
 }parametros;
-int ciclos_ejecucion_0(parametros *v);
-void procesos_listos(parametros **v, parametros *procesos, int ciclos);
-void rol_fifo(parametros *v);
+int ciclos_ejecucion_0(parametros *v, int *size);
+void procesos_listos(parametros *v, parametros *procesos, int ciclos, int *size);
+void rol_fifo(parametros *v, int *size);
 void estadistica(parametros v, parametros *p, int ciclos);
 int main(){
-	int i;
+	int i, k=0, size=0;
 	int ciclos=0;
-	parametros *v=(parametros*)malloc(sizeof(parametros));
-	
+	parametros v[5];
 	// Inicialización de los datos de procesos
 	parametros procesos[NPROC];    
 	strcpy(procesos[0].nombre, "A");
@@ -45,54 +44,71 @@ int main(){
 	printf("\nProceso | t_comienzo | t_final | t_retorno | t_espera\n");
 	printf("-------------------------------------------------------\n");
 	
-	while(ciclos_ejecucion_0(v)==0){
-		procesos_listos(&v, procesos, ciclos);
-		rol_fifo(v);
+	while(k<5){//ciclos_ejecucion_0(v, &size)==0){
+		procesos_listos(v, procesos, ciclos, &size);
+		rol_fifo(v, &size);
 		v[0].t_ejec-=3;
 
 		if(v[0].t_ejec < 0){estadistica(v[0], procesos, ciclos);}
-		//printf("hey-->%i\n", ciclos_ejecucion_0(v));
+		k++;
 	}
+	
 	for(int i=0; i<NPROC; i++){
 			printf("   %s \t    %d \t\t %d \t   %d \t      %d\n", procesos[i].nombre,procesos[i].t_com, procesos[i].t_fin, procesos[i].t_ret, procesos[i].t_esp);
 	}
-	free(v);
 }
-int ciclos_ejecucion_0(parametros *v){
+int ciclos_ejecucion_0(parametros *v, int *size){
 	
-	int n=sizeof(*v)/sizeof(parametros);
 	//printf("hola->%i\n", n);
-	if(n==5){
-		for(int i=0; i<n; i++){
+	if(*size==5){
+		for(int i=0; i<NPROC; i++){
 			if(v[i].t_ejec!=0){return 0;}
 		}
 		return 1;
 	}
 	else{return 0;}
 }
-void procesos_listos(parametros **v, parametros *procesos, int ciclos){
-	int n=sizeof(**v)/sizeof(parametros);	
-	printf("hola->%i\n", n);
+/*void procesos_listos(parametros *v, parametros *procesos, int ciclos){
+	int n=sizeof(**v)/sizeof(parametros);
+	printf("ANTIGUO tamano vector--> %lu\n", sizeof(**v));
 	for(int i=0; i<NPROC; i++){
 		if(ciclos==procesos[i].t_lleg){
-			*v=realloc(*v, (n+1)*sizeof(parametros));		
-			//printf("hola\n");			
+			int size=n+1;
+						
+			if((v=(parametros*)realloc(v, sizeof(parametros)*size))==NULL){
+				perror("error ");
+				exit(-1);
+			}
+			printf("NUEVO tamano vector--> %lu\n", sizeof(**v));	
+						
+			printf("checkpoint 1\n");//asignar			
+						
 			*v[n]=procesos[i];
 			
+			printf("checkpoint 2\n");
 			v[n]->t_com=ciclos;
+			printf("checkpoint 3\n");
 		}
 	}
-}
-void rol_fifo(parametros *v){
-	int n=sizeof(*v)/sizeof(parametros);
-	//printf("hola->%lu\n", sizeof(parametros));
-
-	if(n>1){
+}*/
+void procesos_listos(parametros *v,parametros *procesos,int ciclos, int *size){
+	parametros *aux=NULL;
+	printf("size--->%i\n", *size);
+	for(int i=0;i<NPROC;i++){
+		if(ciclos==procesos[i].t_lleg){
+			v[*size]=procesos[i];
+			v[*size].t_com=ciclos;
+			(*size)++;
+		}
+	}     	
+} 
+void rol_fifo(parametros *v, int *size){
+	if(*size>1){
 		parametros aux=v[0];
-		for(int i=0; i<(n-1); i++){
+		for(int i=0; i<NPROC; i++){
 			v[i]=v[i+1];
 		}
-		v[n-1]=aux;
+		v[NPROC-1]=aux;
 	}
 }
 void estadistica(parametros v, parametros *p, int ciclos){		
