2ac21df1e2fdb54845e3b9510ef75f2eca50f3f2
Daniel Ranchal <danitico98@gmail.com>
2017-12-06 14:34:11 +0100

    Sigo teniendo error en las tablas

--- P4/algoritmo_RR.c
+++ P4/algoritmo_RR.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define RODAJA 3
 #define NPROC 5
 typedef struct params{
     char nombre[10];
@@ -14,11 +15,14 @@ typedef struct params{
 int ciclos_ejecucion_0(parametros *v, int *size);
 void procesos_listos(parametros *v, parametros *procesos, int ciclos, int *size);
 void rol_fifo(parametros *v, int *size);
-void estadistica(parametros v, parametros *p, int ciclos);
+void estadistica(parametros *v, parametros *p, int ciclos);
+void tiempo_comienzo(parametros *v, parametros *p, int ciclos);
+void imprime(parametros *v);
 int main(){
-	int i, k=0, size=0;
+	int i, k=0, size=0, anterior=0;
 	int ciclos=0;
 	parametros v[5];
+	
 	// Inicialización de los datos de procesos
 	parametros procesos[NPROC];    
 	strcpy(procesos[0].nombre, "A");
@@ -41,83 +45,113 @@ int main(){
 	procesos[4].t_ejec = 2;
 	procesos[4].t_lleg = 4;
 	
-	printf("\nProceso | t_comienzo | t_final | t_retorno | t_espera\n");
-	printf("-------------------------------------------------------\n");
-	
-	while(k<5){//ciclos_ejecucion_0(v, &size)==0){
-		procesos_listos(v, procesos, ciclos, &size);
-		rol_fifo(v, &size);
-		v[0].t_ejec-=3;
+		
+	while(ciclos_ejecucion_0(v, &size)==0){
+		for(; anterior<(ciclos+1); anterior++){		
+			procesos_listos(v, procesos, anterior, &size);
+		}
 
-		if(v[0].t_ejec < 0){estadistica(v[0], procesos, ciclos);}
-		k++;
+		anterior=ciclos+1;//para la lista de listos		
+		
+		rol_fifo(v, &size);//rotar el vector
+		tiempo_comienzo(&v[0], procesos, ciclos);	
+		//printf("proceso en ejecucion -->%c\n", v[0].nombre[0]);
+		if(strcmp(v[0].nombre, "A")==0){
+			printf("ciclos restantes-->%i\n", v[0].t_ejec);
+		}
+		v[0].t_ejec-=RODAJA;
+		if(strcmp(v[0].nombre, "A")==0){
+			printf("ciclos restantes después-->%i\n", v[0].t_ejec);
+			//printf("ciclos-->%i\n", ciclos);
+		}	
+		if(v[0].t_ejec <= 0){
+			ciclos+=(v[0].t_ejec + RODAJA);			
+			if(strcmp(v[0].nombre, "A")==0){
+				printf("ciclos-->%i\n", ciclos);
+			}
+			estadistica(&v[0], procesos, ciclos);
+		}
+		else{
+			ciclos+=RODAJA;
+		}
+		if(strcmp(v[0].nombre, "A")==0){
+			printf("uhh ciclos-->%i\n", ciclos);
+		}
+		
+		//imprime(v);
 	}
 	
+	printf("\nProceso | t_comienzo | t_final | t_retorno | t_espera\n");
+	printf("-------------------------------------------------------\n");
+
 	for(int i=0; i<NPROC; i++){
 			printf("   %s \t    %d \t\t %d \t   %d \t      %d\n", procesos[i].nombre,procesos[i].t_com, procesos[i].t_fin, procesos[i].t_ret, procesos[i].t_esp);
 	}
 }
 int ciclos_ejecucion_0(parametros *v, int *size){
-	
-	//printf("hola->%i\n", n);
-	if(*size==5){
-		for(int i=0; i<NPROC; i++){
-			if(v[i].t_ejec!=0){return 0;}
+	if((*size)==5){
+		for(int i=0; i<5; i++){
+			if(v[i].t_ejec > 0){
+				return 0;
+			}
 		}
 		return 1;
 	}
-	else{return 0;}
+	return 0;
 }
-/*void procesos_listos(parametros *v, parametros *procesos, int ciclos){
-	int n=sizeof(**v)/sizeof(parametros);
-	printf("ANTIGUO tamano vector--> %lu\n", sizeof(**v));
-	for(int i=0; i<NPROC; i++){
-		if(ciclos==procesos[i].t_lleg){
-			int size=n+1;
-						
-			if((v=(parametros*)realloc(v, sizeof(parametros)*size))==NULL){
-				perror("error ");
-				exit(-1);
-			}
-			printf("NUEVO tamano vector--> %lu\n", sizeof(**v));	
-						
-			printf("checkpoint 1\n");//asignar			
-						
-			*v[n]=procesos[i];
-			
-			printf("checkpoint 2\n");
-			v[n]->t_com=ciclos;
-			printf("checkpoint 3\n");
-		}
-	}
-}*/
 void procesos_listos(parametros *v,parametros *procesos,int ciclos, int *size){
-	parametros *aux=NULL;
-	printf("size--->%i\n", *size);
+	parametros aux;
 	for(int i=0;i<NPROC;i++){
 		if(ciclos==procesos[i].t_lleg){
-			v[*size]=procesos[i];
-			v[*size].t_com=ciclos;
-			(*size)++;
+			if(*size==4){
+				aux=v[3];
+				v[3]=procesos[i];
+				v[*size]=aux;
+				(*size)++;
+			}		
+			else{
+				v[*size]=procesos[i];
+				(*size)++;
+			}
 		}
 	}     	
 } 
 void rol_fifo(parametros *v, int *size){
-	if(*size>1){
+	//printf("size--->%i\n", *size);
+	if((*size)>1){
 		parametros aux=v[0];
-		for(int i=0; i<NPROC; i++){
+		for(int i=0; i<(NPROC-1); i++){
 			v[i]=v[i+1];
 		}
-		v[NPROC-1]=aux;
+		v[*size-1]=aux;
 	}
 }
-void estadistica(parametros v, parametros *p, int ciclos){		
+void estadistica(parametros *v, parametros *p, int ciclos){		
 	for(int i=0; i<NPROC; i++){
-		if(v.nombre[0]==p[i].nombre[0]){
-			p[i].t_com=v.t_com;
+		if(strcmp(v->nombre, p[i].nombre)==0){
+			v->nombre[1]='T';
+			//printf("proceso %c\n", v->nombre[0]);
+			//printf("ciclos fin-->%i\n", ciclos);
+			p[i].t_com=v->t_com;
 			p[i].t_fin=ciclos;
 			p[i].t_ret=p[i].t_fin - p[i].t_lleg;
 			p[i].t_esp=p[i].t_ret - p[i].t_ejec;
 		}
 	}
+}
+void tiempo_comienzo(parametros *v, parametros *p, int ciclos){
+	for(int i=0; i<NPROC; i++){
+		if(v->nombre[0]==p[i].nombre[0]){
+			if(v->t_ejec==p[i].t_ejec){
+				//printf("ciclo que empieza-->%i\n", ciclos);
+				v->t_com=ciclos;
+			}
+		}
+	}
+}
+void imprime(parametros *v){
+	for(int i=0; i<5; i++){	
+		printf("-->%c %c\n", v[i].nombre[0], v[i].nombre[1]);
+	}
+	printf("\n");
 }
